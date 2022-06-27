14313be2446569f278a0350e71885588530c35a0
Ash Dan <Arvolley01@gmail.com>
2022-06-27 17:04:02 -0400

    added starter pokemon

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 //alert("js is working");
 
 const pokemon = ['charmander','bulbasaur','squirtle'];
+const pokemonIDs = ["004","001","007"];
+
+// loop over every id
+// create html element
+//set html elem values
+//append html obj to the dom
+
+pokemonIDs.map((id) => {
+    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`
+    console.log(imgUrl);
+    let img = document.createElement('img');
+    img.src = imgUrl;
+    img.setAttribute('height','100px')
+    img.setAttribute('width','100px')
+    document.body.append(img);
+});
