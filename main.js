96a54d609a7713f110a9c13d39784c5bb820585c
Ash Dan <Arvolley01@gmail.com>
2022-06-28 13:05:32 -0400

    added button

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ const pokemon = [
     {name: 'Squirtle', id: '007'},
 ];
 
+const newBtn = document.getElementById('new-pokemon-btn');
+newBtn.addEventListener('click', () => {
+    let inputID = prompt('enter pokemon by id');
+    console.log(inputID);
+});
 // loop over every id
 // create html element
 //set html elem values
@@ -23,5 +28,14 @@ pokemon.map((element) => {
     let card = document.createElement('div');
     card.setAttribute('class', "pokemon-card");
     card.append(img, h3);
+    let audioUrl = `https://play.pokemonshowdown.com/audio/cries/${element.name.toLowerCase()}.mp3`
+    let audio = document.createElement('audio');
+    let source = document.createElement('source');
+    source.setAttribute('src', audioUrl);
+    source.setAttribute('type', 'audio/mpeg');
+    audio.append(source);
+    card.addEventListener('click', () => {
+        audio.play();
+    });
     document.getElementById('grid').append(card);
 });
