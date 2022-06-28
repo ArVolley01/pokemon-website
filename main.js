a43d9e8626bd41870abd92af1501e9708f65a796
Ash Dan <Arvolley01@gmail.com>
2022-06-28 11:22:45 -0400

    added object functionality and names to the pictures

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 //alert("js is working");
 
-const pokemon = ['charmander','bulbasaur','squirtle'];
-const pokemonIDs = ["004","001","007"];
+// const pokemon = ['charmander','bulbasaur','squirtle'];
+// const pokemonIDs = ["004","001","007"];
+const pokemon = [
+    {name: 'Charmander', id: '004'},
+    {name: 'Bulbasaur', id: '001'},
+    {name: 'Squirtle', id: '007'},
+];
 
 // loop over every id
 // create html element
 //set html elem values
 //append html obj to the dom
 
-pokemonIDs.map((id) => {
-    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`
-    console.log(imgUrl);
+pokemon.map((element) => {
+    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${element.id}.png`
+    console.log(element);
+    let h3 = document.createElement('h3');
+    h3.innerText = element.name;
     let img = document.createElement('img');
     img.src = imgUrl;
     let card = document.createElement('div');
     card.setAttribute('class', "pokemon-card");
-    card.append(img);
+    card.append(img, h3);
     document.getElementById('grid').append(card);
 });
